f2994be81ea5dfe80958de52418e5c52e7257aef
CosmeTSilva <133916990+CosmeTSilva@users.noreply.github.com>
2024-10-25 18:21:16 -0300

    footer

--- src/components/base/footer/footer.jsx
+++ src/components/base/footer/footer.jsx
@@ -1,9 +1,17 @@
 
-
+import { Rocket } from "lucide-react";
+import { Label } from "@/components/ui/label";
 export default function Footer() {
     return (
-        <div>
-            <footer className="footer   rounded text-orange-500 text-sm p-1 mt-6">SenacX Lavras - 2024</footer>
+        <div className="flex flex-col justify-center">
+
+            <footer className="footer   rounded text-orange-500 text-2xl p-1 mt-12 flex
+             justify-center align-text-bottom">
+                <Rocket className="text-center mr-4 size-12" />
+                <Label className="text-3xl">SenacX Lavras - 2024</Label>
+                <Rocket className="text-center ml-4 size-12" />
+
+            </footer>
         </div>
     );
 };
