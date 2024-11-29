c3c0c4abb9342938f02fa435e5bfce2628b38b51
CosmeTSilva <133916990+CosmeTSilva@users.noreply.github.com>
2024-11-29 18:08:35 -0300

    css footer

--- src/components/base/footer/footer.jsx
+++ src/components/base/footer/footer.jsx
@@ -5,11 +5,11 @@ export default function Footer() {
     return (
         <div className="flex flex-col justify-center">
 
-            <div className="footer   rounded text-orange-500 text-2xl p-1 mt-12 flex
+            <div className="footer   rounded text-orange-500 text-2xl  mb-1 flex
              justify-center align-text-bottom">
                 <Rocket className="text-center mr-4 size-12 text-blue-600" />
-                <Label className="md:text-3xl sm:text-sm text-black">SenacX  - 2024</Label>
-                <Rocket className="text-center ml-4 size-12" />
+                <Label className="md:text-3xl sm:text-sm  text-orange-400">SenacX  - 2024</Label>
+                <Rocket className="text-center ml-4 size-12 text-blue-600" />
 
             </div>
         </div>
